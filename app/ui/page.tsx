495868b2f872d02781a574f5b24dafbe087dd83f
DEAN FREDERICK PURBA <deanfrederick690@gmail.com>
2026-04-11 20:44:35 +0700

    pencobaan

--- app/ui/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-<Image
-    src="/hero-desktop.png"
-    width={1000}
-    height={760}
-    className="hidden md:block"
-    alt="Screenshots of the dashboard project showing desktop version"
-/>
